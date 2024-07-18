7a64fc1105c9639652233fc0771bbea8a2421a32
Jeff <84156730+JeffM-Code@users.noreply.github.com>
2024-07-18 08:58:01 +0100

    new model application

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# ImbalancePricingForecastModel
-New app
+## Imbalance Pricing Forecasting Model<br><br>
+[Model development](https://github.com/JeffM-Code/PortfolioWork/tree/main/ML/ImbalancePricing)<br><br>
+[App](https://imbalancepricingforecastmodel.streamlit.app/)<br><br>
+
+Toggle the predicted vs actual values of model to see how well the model predicts system buy / sell prices. The slider introduces a percentage of the data into the model and populates the values available for the model to forecast based on.<br><br>
+
+#### Illustration:<br><br>
+<img src="example.png" alt="example" width="650"/><br><br>
+
+Generally, it should forecast better, the more data it has to work with, with a few key things to note:<br><br>
+
+```
+lstm_model = Sequential()
+lstm_model.add(LSTM(units=100, return_sequences=True, input_shape=(time_step, 1)))
+lstm_model.add(Dropout(0.2))
+lstm_model.add(LSTM(units=100, return_sequences=True))
+lstm_model.add(Dropout(0.2))
+lstm_model.add(LSTM(units=100))
+lstm_model.add(Dropout(0.2))
+lstm_model.add(Dense(units=50))
+lstm_model.add(Dense(units=1))
+
+
+```
+<br><br>
+
+The number of LSTM units and layers affects the model's ability to learn complex patterns in the data, usually visible in the level of jaggedness or fluctuations in the curve.<br><br>
+
+
+```
+lstm_model.compile(optimizer='adam', loss='mean_squared_error')
+
+```
+<br><br>
+
+The choice of optimizer and loss function is crucial for the model's learning process, as they generally reduce training losses, where different optimizers have different strategies for updating the weights and can have a significant impact on the model's convergence speed and final performance.<br><br>
+
+
+```
+lstm_model.fit(X, y, batch_size=32, epochs=50)
+
+```
+<br><br>
+
+Bach size and epoch parameters control the number of samples processed before the model's parameters are updated, as well as how many times the learning algorithm will work through the entire training dataset respectively.<br><br>
+
+
+### Expected Model Performance:<br><br>
+
+```
+MSE: 914.6547672440631
+RMSE: 30.243259864704783
+MAE: 22.775557182210395
+R2 Score: 0.30823812286315355
+
+```
---
+++ streamlit_app.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import os
+
+def load_and_preprocess_data(file_path):
+    data = pd.read_csv(file_path)
+    data.ffill(inplace=True)
+    data['SettlementDate'] = pd.to_datetime(data['SettlementDate'])
+    data['StartTime'] = pd.to_datetime(data['StartTime'])
+    data.set_index('StartTime', inplace=True)
+    data = data.sort_index()
+    return data
+
+@st.cache_resource
+def load_model_and_scaler(model_path, scaler_data):
+    model = load_model(model_path)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler.fit(scaler_data)
+    return model, scaler
+
+model_file_name = 'LSTM_price.keras'
+
+if not os.path.exists(model_file_name):
+    st.error(f"Failed to find the model file: {model_file_name}.")
+    st.stop()
+
+test_file_path = 'test_1.csv'
+test_data = load_and_preprocess_data(test_file_path)
+test_price_data = test_data[['SystemSellPrice']].values
+
+combined_price_data = test_price_data
+
+model, scaler = load_model_and_scaler(model_file_name, combined_price_data)
+
+def create_dataset(data, time_step=1):
+    X, Y = [], []
+    for i in range(len(data) - time_step - 1):
+        a = data[i:(i + time_step), 0]
+        X.append(a)
+        Y.append(data[i + time_step, 0])
+    return np.array(X), np.array(Y)
+
+time_step = 10
+
+st.title('System Sell / Buy Price Prediction')
+
+data_percentage = st.slider('Select the percentage of test data to use', 1, 100, 50)
+test_data_length = int(len(test_price_data) * (data_percentage / 100))
+
+if st.button('Predict'):
+    if test_data_length <= time_step:
+        st.error("The selected percentage of test data is too low to create sequences for prediction.")
+    else:
+        test_scaled_data = scaler.transform(test_price_data[:test_data_length])
+        X_test, y_test = create_dataset(test_scaled_data, time_step)
+        X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+        test_predictions = model.predict(X_test)
+        test_predictions = scaler.inverse_transform(test_predictions)
+        
+        test_series_adjusted = pd.Series(test_price_data.flatten()[time_step + 1:test_data_length], 
+                                         index=test_data.index[time_step + 1:test_data_length])
+        st.write(f'Predicted System Sell Prices for {data_percentage}% of test data:')
+
+        plt.figure(figsize=(14, 8))
+        plt.plot(test_series_adjusted.index, test_series_adjusted, label='Actual Test Data', color='blue')
+        plt.plot(test_series_adjusted.index, test_predictions[:len(test_series_adjusted)], label='Predicted Test Data', color='green')
+        plt.xlabel('Date')
+        plt.ylabel('System Sell Price')
+        plt.legend()
+        st.pyplot(plt)
+
+if st.checkbox('Show Scatter Plot'):
+    if test_data_length <= time_step:
+        st.error("The selected percentage of test data is too low to create sequences for prediction.")
+    else:
+        test_scaled_data = scaler.transform(test_price_data[:test_data_length])
+        X_test, y_test = create_dataset(test_scaled_data, time_step)
+        X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+        test_predictions = model.predict(X_test)
+        test_predictions = scaler.inverse_transform(test_predictions)
+        
+        test_series_adjusted = pd.Series(test_price_data.flatten()[time_step + 1:test_data_length], 
+                                         index=test_data.index[time_step + 1:test_data_length])
+        plt.figure(figsize=(8, 8))
+        plt.scatter(test_series_adjusted, test_predictions[:len(test_series_adjusted)], color='purple')
+        plt.plot([test_series_adjusted.min(), test_series_adjusted.max()], 
+                 [test_series_adjusted.min(), test_series_adjusted.max()], color='red', linewidth=2)
+        plt.xlabel('Actual Prices')
+        plt.ylabel('Predicted Prices')
+        plt.title('Actual vs Predicted Prices Scatter Plot')
+        st.pyplot(plt)
